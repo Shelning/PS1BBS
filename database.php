a622be377d4cb2eac63d2b2e1ae21fe7f6ab8029
Shoma M <44600386+Shelning@users.noreply.github.com>
2018-11-22 19:37:10 +0900

    Update database.php

--- database.php
+++ database.php
@@ -2,9 +2,9 @@
 session_start();
 setcookie("jeff", "", time() + 604800); //クッキーの有効期限を7日間に設定
 
-$dsn = "mysql:dbname=tt_576_99sv_coco_com;host=localhost" ;
-$db[user] = "tt-576.99sv-coco" ;
-$db[pass] = "tN7ZhXGc" ;
+$dsn = "" ;
+$db[user] = "" ;
+$db[pass] = "" ;
 
 /* HTML特殊文字をエスケープする関数 */
 function h($str) {
